71a685d5505a0c64edc99a7808835d1507d792ee
GuiSantos <gui.ssilva1994@gmail.com>
2026-04-13 22:08:49 -0300

    Understanding the basics of components

--- React Basics/first_vite_project/src/App.tsx
+++ React Basics/first_vite_project/src/App.tsx
@@ -1,7 +1,24 @@
 export default function App() {
-  return(
+  return (
     <div>
-      <h1></h1>
+      <h1>Users</h1>
+      <User name="Maria Green" age={21} />
+      <User name="John Brown" age={44} />
+      <User name="Alex Blue" age={32} />
+    </div>
+  )
+}
+
+interface UserProps {
+  name: string;
+  age: number;
+}
+
+function User(user: UserProps) {
+  return (
+    <div>
+      <h2>User: {user.name}</h2>
+      <h3>Age: {user.age}</h3>
     </div>
   )
 }
